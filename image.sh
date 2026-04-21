4315daf77a6fa88b3430a1ebf6298eb390cd2794
解(xie)先生🌽 <chenglongxie@foxmail.com>
2026-04-22 07:49:37 +0800

    Update custom theme styles

---
+++ image.sh
@@ -0,0 +1,5 @@
+docker tag ghcr.io/9999-12-31/jamovi:latest harbor.bigdata.com/public/jamovi:latest
+
+docker push harbor.bigdata.com/public/jamovi:latest
+
+docker pull ghcr.io/9999-12-31/jamovi:latest
